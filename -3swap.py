4f88b1fb364b9d4a56f582c73861b84ed7190f94
Pooja <poojahippargi67@gmail.com>
2026-07-14 21:26:40 +0530

    message

---
+++ -3swap.py
@@ -0,0 +1,10 @@
+x = 13
+y = 12
+temp = x
+print("the value of temp variable is",temp)
+x  = y
+print("the value of x is",y)
+y = temp
+print("the value of y is",temp)
+
+
---
+++ day2/_1square.py
@@ -0,0 +1,9 @@
+#using expresion
+num1=int(input("enter number"))
+squ=num1**(1/2)
+print("square root of the number",squ)
+#using module
+import math
+num=int(input("enter number"))
+sr=math.sqrt(num)
+print("square root of the give number is",sr)
