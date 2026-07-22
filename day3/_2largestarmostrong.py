579434364b32adba6569fe54644078e09ce7ed8c
Pooja <poojahippargi67@gmail.com>
2026-07-22 21:54:26 +0530

    message

---
+++ day3/_2largestarmostrong.py
@@ -0,0 +1,9 @@
+num1=12
+num2=14
+num3=16
+if (num1 > num2 and num1>num3):
+    print("largest number")
+elif (num2>num3 and num2>num1):
+    print("largest number")
+else:
+    print("not largest number")
